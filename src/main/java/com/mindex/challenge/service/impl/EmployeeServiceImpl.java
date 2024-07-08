b1db8f98e87e3c9fd0cb1f50f072b66dc2d66e84
mtuchols <56408401+mtuchols@users.noreply.github.com>
2024-07-07 21:47:15 -0400

    Fix employee endpoint issue with null direct report values

--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -3,11 +3,14 @@ package com.mindex.challenge.service.impl;
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.service.EmployeeService;
+import jdk.internal.org.jline.utils.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -23,7 +26,6 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         employee.setEmployeeId(UUID.randomUUID().toString());
         employeeRepository.insert(employee);
-
         return employee;
     }
 
@@ -32,18 +34,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         LOG.debug("Finding employee with id [{}]", id);
 
         Employee employee = employeeRepository.findByEmployeeId(id);
-
+        if (employee.getDirectReports() != null) {
+            List <Employee> fullyPopulatedEmployeeList = new ArrayList<>();
+            for (Employee directReportEmployee : employee.getDirectReports()) {
+                // TODO : Is there a better way to  fill in this data instead of querying for every direct report?
+                fullyPopulatedEmployeeList.add(read(directReportEmployee.getEmployeeId()));
+            }
+            employee.setDirectReports(fullyPopulatedEmployeeList);
+        }
         if (employee == null) {
             throw new RuntimeException("Invalid employeeId: " + id);
         }
-
         return employee;
     }
 
     @Override
     public Employee update(Employee employee) {
         LOG.debug("Updating employee [{}]", employee);
-
         return employeeRepository.save(employee);
     }
 }
